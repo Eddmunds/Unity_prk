71cdd99781f7ca0ad326265f2ae356a83c09a6ec
Eddmunds Toms <eddmunds.toms@gmail.com>
2019-05-09 23:22:53 +0300

    new vers

---
+++ Edmunds_Tomins_labirints/Assets/skripti/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement; //tiek izmantots lai piekļūtu ainu funkcijām
+
+public class Menu : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        EditorSceneManager.LoadScene(1);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void toMain()
+    {
+        EditorSceneManager.UnloadSceneAsync("SampleScene");
+        EditorSceneManager.LoadScene(0);
+    }
+
+}
---
+++ Edmunds_Tomins_labirints/Assets/skripti/ZombieKustiba.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ZombieKustiba : MonoBehaviour {
+	public Transform target;
+	public Transform myTransform;
+    // Update is called once per frame
+    void Update()
+    {
+
+    	transform.LookAt(target);
+    	transform.Translate(Vector3.forward*1*Time.deltaTime);
+        
+    }
+}
